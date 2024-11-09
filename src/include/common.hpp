fe18f95a5397148a09749f02ae09d7a45ec6e2e3
Electron7-7 <teahaile2@teahaile.com>
2024-11-08 20:39:25 -0500

    Bringing over camera class from "LearnOpenGL" branch; after this, I'm going to be keeping all LearnOpenGL code in its branch and "trunk" will house my personal project

--- src/include/common.hpp
+++ src/include/common.hpp
@@ -1,16 +1,19 @@
-#ifndef ALL_INCLUDES
-#define ALL_INCLUDES
+#ifndef GL_INCLUDES
+#define GL_INCLUDES
 	#include <glad/glad.h>
 	#include <GLFW/glfw3.h>
 	#include <glm/glm.hpp>
 	#include <glm/gtc/matrix_transform.hpp>
 	#include <glm/gtc/type_ptr.hpp>
-	#include <string>
-	#include <math.h>
 #endif
 
-#ifndef FUNCTIONS_COMMON_INCLUDED
-#define FUNCTIONS_COMMON_INCLUDED
+#ifndef COMMON_INCLUDES
+#define COMMON_INCLUDES
+	#include <string>
+#endif
+
+#ifndef COMMON_FUNCTIONS
+#define COMMON_FUNCTIONS
 
 void shaderErrorHandler(unsigned int shader, bool is_program = false);
 
@@ -32,4 +35,48 @@ public:
 	void setInt(const std::string &name, int value) const;
 	void setFloat(const std::string &name, float value) const;
 };
+#endif
+
+#ifndef CAMERA_CLASS
+#define CAMERA_CLASS
+
+enum Camera_Movement
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+
+class Camera
+{
+public:
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+
+	float Yaw;
+	float Pitch;
+
+	float MovementSpeed;
+	float MouseSensitivity;
+
+	Camera(glm::vec3 position = glm::vec3(0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+
+	glm::mat4 GetViewMatrix();
+	void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch);
+
+private:
+	void updateCameraVectors();
+};
+
 #endif
