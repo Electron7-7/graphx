3d579952fe8b7132eb345c2a8c77c229bc8f0054
Electron7-7 <teahaile2@teahaile.com>
2025-03-17 16:33:25 -0400

    reverting to old commit; time to start from "scratch" on this problem

--- src/g_imgui.hpp
+++ src/g_imgui.hpp
@@ -1,16 +1,22 @@
 #ifndef GRAPHX_IMGUI
 #define GRAPHX_IMGUI
-#include "sanity.hpp" // Included for the GLM header files
 #include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include <glm/fwd.hpp>
+#include <string>
+#include <map>
 
 // Forward Declarations
 struct Theatre;
+struct gMeshData;
 
 struct GraphXConsole
 {
 	bool active;
 	bool secondary_active;
 	bool tertiary_active;
+	bool fourth_active;
+
 	ImGuiKey toggle_activation_key;
 	std::string name;
 	Theatre *current_theatre = nullptr;
@@ -20,9 +26,13 @@ struct GraphXConsole
 	void updateFrame(GLFWwindow *window);
 	void displayTheatre();
 	void displayTheatreData();
+	void displayMeshData();
 	bool justClosed();
+	std::string getMeshData(std::pair<const std::string, gMeshData> &mesh_data);
 
 private:
+	std::map<std::string, gMeshData> mesh_data_store;
+	std::map<std::string, std::string> mesh_data_printouts;
 	bool was_active;
 };
 
